edbb0cb8004e1b6af47f11316734f88fb42a86f0
Aryan Mishra <aryantiny29@gmail.com>
2026-02-05 11:23:48 +0530

    Serve index.html at root

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
-import express, { json } from 'express'
+import express from 'express'
 import { generate } from './chat.js'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 const app = express()
 const port = process.env.PORT || 3001
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(cors())
-
 app.use(express.json())
+app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  res.send('chat')
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.post('/', async (req, res) => {
-          const {message, id} = req.body
+  const { message, id } = req.body
 
-          if(!message || !id) {
-                    return res.status(400).json({message : "All fields are required !!"})
-          }
+  if (!message || !id) {
+    return res.status(400).json({ message: 'All fields are required !!' })
+  }
 
-          console.log(message)
+  console.log(message)
 
-          const result = await generate(message, id)
+  const result = await generate(message, id)
 
-          res.json({message : result, id : id})
+  res.json({ message: result, id })
 })
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+  console.log('App running on port ' + port)
 })
-
